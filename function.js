6b5aabd7b85f2ca248f355f865e1b460119aaea7
Grader <labcontext@gmail.com>
2017-01-24 13:02:31 +0400

    countChar

--- function.js
+++ function.js
@@ -58,3 +58,35 @@ isEven(25);
 isEven(20);
 isEven(16);
 isEven(15);
+
+/*
+Напишите функцию countBs, которая принимает строку в качестве аргумента, и возвращает количество символов “B”, содержащихся в строке.
+Затем напишите функцию countChar, которая работает примерно как countBs, только принимает второй параметр — символ, который мы будем искать в строке
+*/
+const countBs = (str) => {
+	const iter = (i, acc) => {
+		if (i > str.length - 1) {
+			return acc;
+		}
+		const newAcc = str.charAt(i) === 'B' ? acc + 1 : acc;
+		return iter(i + 1, newAcc);
+	}
+	return iter(0, 0);
+}
+console.log(countBs('BarBara'));
+console.log(countBs('garBaraBB'));
+console.log(countBs('BBarBarassB B dB'));
+
+const countChar = (str, symb) => {
+	const iter = (i, acc) => {
+		if (i > str.length - 1) {
+			return acc;
+		}
+		const newAcc = str.charAt(i) === symb ? acc + 1 : acc;
+		return iter(i + 1, newAcc);
+	}
+	return iter(0, 0);
+}
+console.log(countChar('BarBara', 'a'));
+console.log(countChar('garBaraBB', 'B'));
+console.log(countChar('BBarBarassB B dB', 'r'));
